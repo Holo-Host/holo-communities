a62610fc26c4e77adedc0bcbf46f830786b5a834
Loren Johnson <loren@hellovenado.com>
2020-02-11 15:47:35 -0800

    Add WS subdomain modifier (e.g. hc3400.localhost:9000) for multi-agent testing

--- src/util/holochain.js
+++ src/util/holochain.js
@@ -1,19 +1,15 @@
+export const HOLOCHAIN_ACTIVE = true
 export const HOLOCHAIN_HASH_MATCH = '[a-zA-Z0-9]{46}'
 export const HOLOCHAIN_POLL_INTERVAL_SLOW = 25000
 export const HOLOCHAIN_POLL_INTERVAL_FAST = 5000
-export const HOLOCHAIN_DEFAULT_COMMUNITY_SLUG = 'hylo-holochain'
-export const HOLOCHAIN_WEBSOCKET_URI = 'ws://localhost:8889'
-export const HOLOCHAIN_SUBDOMAINS = [
-  'holo',
-  'holochain'
-]
-export const HOLOCHAIN_ACTIVE = true
-// process.env.HOLOCHAIN_BUILD || (
-//   typeof window !== 'undefined' &&
-//     HOLOCHAIN_SUBDOMAINS.some(subdomain => window.location.host.split('.')[0] === subdomain)
-// )
 
-export const DISABLED_IN_HOLOCHAIN = HOLOCHAIN_ACTIVE
+const conductorPortMatch = window.location.host.split('.')[0].match(/\d{4,6}/)
+
+export const HOLOCHAIN_DEFAULT_COMMUNITY_SLUG = 'hylo-holochain'
+export const HOLOCHAIN_WEBSOCK_URI_DEFAULT = 'ws://localhost'
+export const HOLOCHAIN_WEBSOCK_PORT_DEFAULT = '3400'
+export const HOLOCHAIN_WEBSOCKET_URI = HOLOCHAIN_WEBSOCK_URI_DEFAULT + ':' +
+  (conductorPortMatch ? conductorPortMatch[0] : HOLOCHAIN_WEBSOCK_PORT_DEFAULT)
 
 export function currentDateString () {
   return new Date().toISOString()
